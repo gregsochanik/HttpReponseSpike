a81170d45bf6602ecbb295e758879657c8bb51c8
Greg Sochanik <greg@sochanik.com>
2010-11-07 12:58:20 +0000

    Added a fluent style WebHeaderBuilder

---
+++ src/HttpResponseLibrary/WebHeaderBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace HttpResponseLibrary
+{
+    public class WebHeaderBuilder
+    {
+        readonly WebHeaderCollection _headers;
+
+        public WebHeaderBuilder()
+        {
+            _headers = new WebHeaderCollection();
+        }
+
+        public WebHeaderBuilder AddHeader(string name, string value)
+        {
+            _headers.Add(name, value);
+            return this;
+        }
+
+        public WebHeaderCollection Build()
+        {
+            return _headers;
+        }
+    }
+}
